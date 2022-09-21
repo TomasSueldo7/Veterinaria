cca7949638ba25cbc059f69ede0a71385dac75d6
TomasSueldo7 <tsueldo99@gmail.com>
2022-09-20 23:12:46 -0300

    Creacion Proyecto

---
+++ Grupo4_PAVI_Veterinaria/Principal.cs
@@ -0,0 +1,10 @@
+namespace Grupo4_PAVI_Veterinaria
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+    }
+}
